89b26f9fbbec19ba0613ced33f58d9162860e426
MaohuaFan <9019910@student.zadkine.nl>
2024-10-18 11:42:35 +0200

    Stemgerechtigde US

--- src/Stemgerechtigde/insertStemgerechtigde.php
+++ src/Stemgerechtigde/insertStemgerechtigde.php
@@ -5,31 +5,45 @@
 require_once '../../vendor/autoload.php';
 
 use Examenopdracht\classes\Stemgerechtigde;
-use Examenopdracht\classes\Verkiezing;
-
-$verkiezingen = new Verkiezing("", "", "", 0); // Je kunt de parameters hier negeren omdat we ze later instellen
-
-// Verkrijg de verkiezing
-$verkiezing = $verkiezingen->getVerkiezing();
 
 if (isset($_POST["insert"]) && $_POST["insert"] == "Registreren") {
-    $identificatienummer = $_POST['identificatienummer'];
+    $bsnNummer = $_POST['bsnNummer'];
     $naam = $_POST['naam'];
+    $wachtwoord = $_POST['wachtwoord'];
     $adres = $_POST['adres'];
     $geboortedatum = $_POST['geboortedatum'];
-    $verkiezingId = $_POST['verkiezing_id'];
 
-    // Maak een instantie van Stemgerechtigde
-    $stemgerechtigde = new Stemgerechtigde($identificatienummer, $naam, $adres, $geboortedatum, $verkiezingId);
-
-    // Roep de registreer-methode aan
-    $insertedId = $stemgerechtigde->registreerStemgerechtigde();
-
-    if ($insertedId !== false) {
-        echo "Stemgerechtigde geregistreerd! ID is: $insertedId";
-    } else {
-        echo "Fout bij registratie.";
+    // Validatie van het BSN-nummer (moet 9 cijfers zijn)
+    if (isset($_POST["insert"]) && $_POST["insert"] == "Registreren") {
+        $bsnNummer = $_POST['bsnNummer'];
+        $naam = $_POST['naam'];
+        $wachtwoord = $_POST['wachtwoord'];
+        $adres = $_POST['adres'];
+        $geboortedatum = $_POST['geboortedatum'];
+    
+        // Validatie van het BSN-nummer (moet 9 cijfers zijn)
+        if (!preg_match('/^\d{9}$/', $bsnNummer)) {
+            echo "Het BSN-nummer moet precies 9 cijfers bevatten.";
+        } else {
+            // Controleer of het BSN-nummer al bestaat
+            if (Stemgerechtigde::exists($bsnNummer)) {
+                echo "Fout: Dit BSN-nummer is al geregistreerd.";
+            } else {
+                // Maak een instantie van Stemgerechtigde
+                $stemgerechtigde = new Stemgerechtigde($bsnNummer, $naam, $wachtwoord, $adres, $geboortedatum);
+    
+                // Roep de registreer-methode aan
+                $insertedId = $stemgerechtigde->registreerStemgerechtigde();
+    
+                if ($insertedId !== false) {
+                    echo "Stemgerechtigde geregistreerd! ID is: $insertedId";
+                } else {
+                    echo "Fout bij registratie.";
+                }
+            }
+        }
     }
+    
 }
 ?>
 
@@ -44,20 +58,20 @@ if (isset($_POST["insert"]) && $_POST["insert"] == "Registreren") {
 <body>
     <h1>Registreer Stemgerechtigde</h1>
     <form method="post">
-        <label for="identificatienummer">Identificatienummer:</label>
-        <input type="text" id="identificatienummer" name="identificatienummer" required/><br>
+        <label for="bsnNummer">BSN-Nummer:</label>
+        <input type="text" id="bsnNummer" name="bsnNummer" required/><br>
 
         <label for="naam">Naam:</label>
         <input type="text" id="naam" name="naam" required/><br>
+
+        <label for="wachtwoord">Wachtwoord:</label>
+        <input type="password" id="wachtwoord" name="wachtwoord" required/><br>
 
         <label for="adres">Adres:</label>
         <input type="text" id="adres" name="adres"/><br>
 
         <label for="geboortedatum">Geboortedatum:</label>
         <input type="date" id="geboortedatum" name="geboortedatum" required/><br>
-
-        <label for="verkiezing_id">Verkiezing ID:</label>
-        <?= $verkiezingen->Dropdown_Verkiezing(isset($verkiezingId) ? $verkiezingId : null); ?>
 
         <input type="submit" name="insert" value="Registreren">
     </form>
